7d4496a857ae03dabb5a6dacdfd259b03854ff16
Mike Hayes <55102399+mikechayes-AF@users.noreply.github.com>
2021-09-21 16:10:04 -0400

    For Konrad

--- src/components/layouts/TheSidebar.vue
+++ src/components/layouts/TheSidebar.vue
@@ -93,28 +93,6 @@
             <DisclosurePanel>
               <ul>
                 <li>
-                  <Disclosure v-slot="{ open }" :default-open="isUserActive">
-                    <DisclosureButton
-                      class="pl-8 pr-4 py-3 flex items-center w-full hover:bg-gray-700"
-                      :class="open ? 'bg-gray-700' : ''"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      </svg>
-                      Big Queue
-                      <span class="ml-auto">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          :class="open ? 'transform rotate-90' : ''"
-                        >
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                        </svg>
-                      </span>
-                    </DisclosureButton>
                     <DisclosurePanel>
                       <ul>
                         <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
@@ -131,14 +109,12 @@
                               >
                                 <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                               </svg>
-                              Tenants
+                              Big Q
                             </a>
                           </li>
                         </router-link>
                       </ul>
-                    </DisclosurePanel>
-                  </Disclosure>
-                </li>
+                    </DisclosurePanel>                 </li>
               </ul>
 
             </DisclosurePanel>
@@ -182,28 +158,6 @@
             <DisclosurePanel>
               <ul>
                 <li>
-                  <Disclosure v-slot="{ open }" :default-open="isUserActive">
-                    <DisclosureButton
-                      class="pl-8 pr-4 py-3 flex items-center w-full hover:bg-gray-700"
-                      :class="open ? 'bg-gray-700' : ''"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      </svg>
-                      Emma
-                      <span class="ml-auto">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          :class="open ? 'transform rotate-90' : ''"
-                        >
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                        </svg>
-                      </span>
-                    </DisclosureButton>
                     <DisclosurePanel>
                       <ul>
                         <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
@@ -220,33 +174,13 @@
                               >
                                 <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                               </svg>
-                              Tenants
+                              Emma
                             </a>
                           </li>
                         </router-link>
                       </ul>
                     </DisclosurePanel>
-                    <DisclosureButton
-                      class="pl-8 pr-4 py-3 flex items-center w-full hover:bg-gray-700"
-                      :class="open ? 'bg-gray-700' : ''"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      </svg>
-                      Salesforce
-                      <span class="ml-auto">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          :class="open ? 'transform rotate-90' : ''"
-                        >
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                        </svg>
-                      </span>
-                    </DisclosureButton>
+
                     <DisclosurePanel>
                       <ul>
                         <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
@@ -263,13 +197,12 @@
                               >
                                 <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                               </svg>
-                              Tenants
+                              Salesforce
                             </a>
                           </li>
                         </router-link>
                       </ul>
-                    </DisclosurePanel>                    
-                  </Disclosure>
+                    </DisclosurePanel> 
                 </li>
               </ul>
 
@@ -314,28 +247,6 @@
             <DisclosurePanel>
               <ul>
                 <li>
-                  <Disclosure v-slot="{ open }" :default-open="isUserActive">
-                    <DisclosureButton
-                      class="pl-8 pr-4 py-3 flex items-center w-full hover:bg-gray-700"
-                      :class="open ? 'bg-gray-700' : ''"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      </svg>
-                      Emma
-                      <span class="ml-auto">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          :class="open ? 'transform rotate-90' : ''"
-                        >
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                        </svg>
-                      </span>
-                    </DisclosureButton>
                     <DisclosurePanel>
                       <ul>
                         <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
@@ -352,36 +263,16 @@
                               >
                                 <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                               </svg>
-                              Tenants
+                              Emma
                             </a>
                           </li>
                         </router-link>
                       </ul>
                     </DisclosurePanel>
-                    <DisclosureButton
-                      class="pl-8 pr-4 py-3 flex items-center w-full hover:bg-gray-700"
-                      :class="open ? 'bg-gray-700' : ''"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-                      </svg>
-                      Salesforce
-                      <span class="ml-auto">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          :class="open ? 'transform rotate-90' : ''"
-                        >
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-                        </svg>
-                      </span>
-                    </DisclosureButton>
+
                     <DisclosurePanel>
                       <ul>
-                        <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
+                        <router-link v-slot="{ isExactActive, href, navigate }" to="/emailSalesforce" custom>
                           <li
                             class="pl-12"
                             :class="[isExactActive ? 'bg-gray-500 text-gray-800' : 'hover:bg-gray-700']"
@@ -395,13 +286,13 @@
                               >
                                 <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
                               </svg>
-                              Tenants
+                              Salesforce
                             </a>
                           </li>
                         </router-link>
                       </ul>
-                    </DisclosurePanel>                    
-                  </Disclosure>
+                    </DisclosurePanel> 
+                   
                 </li>
               </ul>
 
@@ -435,95 +326,6 @@
           </a>
         </li>
 
-        <li class="px-4 py-2 mt-2 text-xs uppercase tracking-wider text-gray-500 font-bold">Apps</li>
-
-        <li class="px-4 cursor-pointer hover:bg-gray-700">
-          <a href="#" class="py-2 flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
-            Messages
-            <span class="ml-auto text-xs bg-gray-500 px-2 py-1 rounded-sm">16</span>
-          </a>
-        </li>
-
-        <li class="px-4 cursor-pointer hover:bg-gray-700">
-          <a href="#" class="py-2 flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            Calendar
-          </a>
-        </li>
-
-        <li class="px-4 py-2 mt-2 text-xs uppercase tracking-wider text-gray-500 font-bold">UI Elements</li>
-
-        <li class="px-4 cursor-pointer hover:bg-gray-700">
-          <router-link :to="{ name: 'card' }" class="py-2 flex items-center">
-            <svg class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M5 3C3.89543 3 3 3.89543 3 5V7C3 8.10457 3.89543 9 5 9H7C8.10457 9 9 8.10457 9 7V5C9 3.89543 8.10457 3 7 3H5Z"
-                fill="currentColor"
-              />
-              <path
-                d="M5 11C3.89543 11 3 11.8954 3 13V15C3 16.1046 3.89543 17 5 17H7C8.10457 17 9 16.1046 9 15V13C9 11.8954 8.10457 11 7 11H5Z"
-                fill="currentColor"
-              />
-              <path
-                d="M11 5C11 3.89543 11.8954 3 13 3H15C16.1046 3 17 3.89543 17 5V7C17 8.10457 16.1046 9 15 9H13C11.8954 9 11 8.10457 11 7V5Z"
-                fill="currentColor"
-              />
-              <path
-                d="M11 13C11 11.8954 11.8954 11 13 11H15C16.1046 11 17 11.8954 17 13V15C17 16.1046 16.1046 17 15 17H13C11.8954 17 11 16.1046 11 15V13Z"
-                fill="currentColor"
-              />
-            </svg>
-            Card
-          </router-link>
-        </li>
-        <li class="px-4 py-2 mt-2 text-xs uppercase tracking-wider text-gray-500 font-bold">Pages</li>
-
-        <li class="px-4 cursor-pointer hover:bg-gray-700">
-          <router-link :to="{ name: 'login' }" class="py-2 flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-              />
-            </svg>
-            Login
-          </router-link>
-        </li>
       </ul>
     </nav>
   </aside>
@@ -545,13 +347,13 @@ export default {
     const route = useRoute()
 
     const isUserManagementActive = computed(() => {
-      const names = ['user-list', 'user-detail', 'roles', 'permissions']
+      const names = ['email-Salesforce', 'user-list', 'user-detail', 'roles', 'permissions']
 
       return names.includes(route.name)
     })
 
     const isUserActive = computed(() => {
-      const names = ['user-list', 'user-detail']
+      const names = ['email-Salesforce', 'user-list', 'user-detail']
 
       return names.includes(route.name)
     })
